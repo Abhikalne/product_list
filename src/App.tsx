ffc21d87c957218ea6aadae71a35fbebc78705e8
Abhijit Kalne <abhikalne1@gmail.com>
2025-08-27 15:13:19 +0530

    hashrouter to provide routeing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import './App.css';
-import Home from './pages/Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ProductList from './pages/ProductList';
-import ErrorBoundary from './ErrorBoundary';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import ProductList from "./pages/ProductList";
+import ErrorBoundary from "./ErrorBoundary";
 
 function App() {
   return (
     <ErrorBoundary>
       <Router>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/product-list' element={<ProductList />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/product-list" element={<ProductList />} />
         </Routes>
       </Router>
     </ErrorBoundary>
